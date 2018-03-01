1944b1a99f22a743dedfe951cdd7e262871f933d
Mega Drive <Mega-Drive@users.noreply.github.com>
2018-03-01 18:56:53 +0100

    Add files via upload

--- js/about_custom.js
+++ js/about_custom.js
@@ -9,6 +9,7 @@
 3. Initialize Hamburger
 4. Init Parallax
 5. Initialize Milestones
+6. Init SVG
 
 
 ******************************/
@@ -44,6 +45,7 @@ $(document).ready(function()
 	initHamburger();
 	initParallax();
 	initMilestones();
+	initSvg();
 
 	/* 
 
@@ -221,5 +223,43 @@ $(document).ready(function()
 			    .addTo(ctrl);
 	    	});
 		}
+	}
+
+	/* 
+
+	6. Init SVG
+
+	*/
+
+	function initSvg()
+	{
+		jQuery('img.svg').each(function()
+		{
+			var $img = jQuery(this);
+			var imgID = $img.attr('id');
+			var imgClass = $img.attr('class');
+			var imgURL = $img.attr('src');
+
+			jQuery.get(imgURL, function(data)
+			{
+				// Get the SVG tag, ignore the rest
+				var $svg = jQuery(data).find('svg');
+
+				// Add replaced image's ID to the new SVG
+				if(typeof imgID !== 'undefined') {
+				$svg = $svg.attr('id', imgID);
+				}
+				// Add replaced image's classes to the new SVG
+				if(typeof imgClass !== 'undefined') {
+				$svg = $svg.attr('class', imgClass+' replaced-svg');
+				}
+
+				// Remove any invalid XML tags as per http://validator.w3.org
+				$svg = $svg.removeAttr('xmlns:a');
+
+				// Replace image with new SVG
+				$img.replaceWith($svg);
+			}, 'xml');
+		});
 	}
 });
